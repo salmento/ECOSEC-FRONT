8793baa67c0763f9d90a76e148844b2ae349e80f
salmento <salmentochitlango@gmail.com>
2022-01-19 19:57:32 +0200

    css format

--- src/layout/AuthLayout.vue
+++ src/layout/AuthLayout.vue
@@ -1,23 +1,6 @@
 <template>
-  <div class="main-content bg-default">
-    <!-- Header -->
-    <div class="header bg-gradient-success py-7 py-lg-8">
-      <div class="separator separator-bottom separator-skew zindex-100">
-        <svg
-          x="0"
-          y="0"
-          viewBox="0 0 2560 100"
-          preserveAspectRatio="none"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <polygon
-            class="fill-default"
-            points="2560 0 2560 100 0 100"
-          ></polygon>
-        </svg>
-      </div>
-    </div>
+  <div class="main-content bg-default h-100">
+    <div class="header bg-gradient-success py-7 py-lg-8"></div>
     <!-- Page content -->
     <div class="container mt--8 pb-5">
       <router-view></router-view>
