01e464804acf6624f17c7fa0d1873fef0c126536
Salmento Chitlango <salmentochitlango@gmail.com>
2023-03-11 19:37:52 +0200

    removing unnecessary files

--- src/plugins/globalDirectives.js
+++
@@ -1,9 +0,0 @@
-import clickOutside from "../directives/click-ouside.js";
-
-const GlobalDirectives = {
-  install(Vue) {
-    Vue.directive("click-outside", clickOutside);
-  },
-};
-
-export default GlobalDirectives;
